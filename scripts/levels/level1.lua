54f99da05f55f3bcccac4b4ce43081e25720e5b9
Joakim Wennergren <joakim.wennergren@databeams.se>
2025-03-16 15:55:48 +0100

    first commit

--- scripts/levels/level1.lua
+++ scripts/levels/level1.lua
@@ -1,75 +1,135 @@
-local Primitives = require("entropy/primitives")
-local Globals = require("scripts/globals/globals")
-
 local Level1 = {}
+
+local mushroom_positions = {
+    -- First layer
+    { x = 5,  y = 8,  z = 41 },
+    { x = 10, y = 7,  z = 41 },
+    { x = 15, y = 7,  z = 41 },
+    { x = 20, y = 11, z = 41 },
+    { x = 25, y = 13, z = 41 },
+    { x = 30, y = 11, z = 41 },
+    { x = 35, y = 11, z = 41 },
+    { x = 40, y = 12, z = 41 },
+    { x = 45, y = 10, z = 41 },
+    { x = 50, y = 9,  z = 41 },
+    { x = 55, y = 6,  z = 41 },
+    { x = 60, y = 6,  z = 41 },
+    { x = 65, y = 7,  z = 41 },
+    { x = 70, y = 9,  z = 41 },
+    { x = 75, y = 6,  z = 41 },
+    { x = 80, y = 5,  z = 41 },
+    { x = 85, y = 4,  z = 41 },
+    { x = 90, y = 4,  z = 41 },
+
+    -- Second layer
+    { x = 5,  y = 20, z = 31 },
+    { x = 10, y = 20, z = 31 },
+    { x = 15, y = 18, z = 31 },
+    { x = 20, y = 16, z = 31 },
+    { x = 25, y = 14, z = 31 },
+    { x = 30, y = 15, z = 31 },
+    { x = 35, y = 16, z = 31 },
+    { x = 40, y = 17, z = 31 },
+    { x = 45, y = 17, z = 31 },
+    { x = 50, y = 18, z = 31 },
+    { x = 55, y = 18, z = 31 },
+    { x = 60, y = 17, z = 31 },
+    { x = 65, y = 16, z = 31 },
+    { x = 70, y = 16, z = 31 },
+    { x = 75, y = 17, z = 31 },
+    { x = 80, y = 14, z = 31 },
+    { x = 85, y = 10, z = 31 },
+    { x = 90, y = 10, z = 31 },
+
+    -- Third layer
+    { x = 10, y = 40, z = 21 },
+    { x = 15, y = 41, z = 21 },
+    { x = 20, y = 32, z = 21 },
+    { x = 25, y = 31, z = 21 },
+    { x = 30, y = 33, z = 21 },
+    { x = 35, y = 34, z = 21 },
+    { x = 40, y = 35, z = 21 },
+    { x = 45, y = 34, z = 21 },
+    { x = 50, y = 34, z = 21 },
+    { x = 55, y = 34, z = 21 },
+    { x = 60, y = 34, z = 21 },
+    { x = 65, y = 35, z = 21 },
+    { x = 70, y = 34, z = 21 },
+    { x = 75, y = 33, z = 21 },
+    { x = 90, y = 31, z = 21 },
+}
 
 function Level1:new()
     local obj = {
-        hasGeneratedBackgrounds = false
+        layer1 = CreateSprite("layer1.png"),
+        layer2 = CreateSprite("layer2.png"),
+        layer3 = CreateSprite("layer3.png"),
+        layer4 = CreateSprite("layer4.png"),
+        layer5 = CreateSprite("layer5.png"),
+        flare = CreateSprite("flare.png"),
+        opacity = 0.0,
+        flare_opacity = 0.0,
+        flare_time = 0.0,
+        should_end = false
     }
-
-    self:PlaceTrees()
-    self:PlaceMushrooms()
-
     setmetatable(obj, self)
     self.__index = self
     return obj
 end
 
-function Level1:CreateSprite(p, x, y, z, w, h)
-    local sprite = Primitives.CreateSprite(Globals.BaseDir .. p)
-    Translate(sprite, x, y, z)
-    Scale(sprite, w, h, 0.0)
-    return sprite
+function clamp(value, min, max)
+    return math.max(min, math.min(max, value))
 end
 
-function Level1:GenerateBackgrounds(width, height)
-    if not self.hasGeneratedBackgrounds then
-        background = Primitives.CreateSprite(Globals.BaseDir .. "resources/sprites/background_4.png")
-        quad = Primitives.CreateQuad()
-        self.hasGeneratedBackgrounds = true
-    end
-    Scale(background, width * 6.0, height - 400.0, 0)
-    Scale(quad, width * 8.0, height * 2.0, 0)
-    Translate(quad, 0, 0, 0)
-    SetColor(quad, 107.0 / 255.0, 213.0 / 255.0, 242.0 / 237.0, 1.0)
-    Translate(background, width, height - 400.0, 1)
-    Rotate(background, 0, 1, 0, 180)
-end
-
-function Level1:PlaceTrees()
-    local paths = {
-        "resources/sprites/tree_4.png",
-        "resources/sprites/tree_6.png"
-    }
-    for i = 1, 20 do
-        local scale = math.random(300, 600)
-        self:CreateSprite(paths[math.random(1, #paths)], 500 + i * math.random(500, 2000), 400, 10, scale, scale + 100)
-    end
-end
-
-function Level1:PlaceMushrooms()
-    local paths = {
-        "resources/sprites/mushroom_1.png",
-    }
-    for i = 1, 20 do
-        local scale = math.random(60, 100)
-        self:CreateSprite(paths[math.random(1, #paths)], 500 + i * math.random(500, 2000), 60, 10, scale, scale)
-    end
-end
-
-function Level1:PlaceForegroundGrass()
-    local paths = {
-        "resources/sprites/grass_4.png",
-    }
-    for i = 1, 20 do
-        local scale = math.random(60, 100)
-        self:CreateSprite(paths[math.random(1, #paths)], 500 + i * math.random(500, 2000), 60, 10, scale, scale)
-    end
+function Level1:stop()
+    self.should_end = true
 end
 
 function Level1:update(deltaTime, width, height)
-    self:GenerateBackgrounds(width, height)
+    if self.should_end and self.opacity > 0.0 then
+        self.opacity = self.opacity - 0.03
+    end
+
+    if self.should_end == false and self.opacity < 1.0 then
+        self.opacity = self.opacity + 0.03
+    else
+        self.flare_time    = self.flare_time + 0.008
+        self.flare_opacity = clamp(EaseInOutSine(self.flare_time), 0.0, 0.4)
+    end
+
+    Scale(self.layer1, width / 2.0, height / 2.0, 0)
+    Translate(self.layer1, width / 2.0, height / 2.0, 10.0)
+    Rotate(self.layer1, 0.0, 1.0, 0.0, 180)
+    SetColor(self.layer1, 1.0, 1.0, 1.0, self.opacity)
+
+    Scale(self.layer2, width / 2.0, height / 2.0, 0)
+    Translate(self.layer2, width / 2.0, height / 2.0, 20.0)
+    Rotate(self.layer2, 0.0, 1.0, 0.0, 180)
+    SetColor(self.layer2, 1.0, 1.0, 1.0, self.opacity)
+
+    Scale(self.layer3, width / 2.0, height / 2.0, 0)
+    Translate(self.layer3, width / 2.0, height / 2.0, 30.0)
+    Rotate(self.layer3, 0.0, 1.0, 0.0, 180)
+    SetColor(self.layer3, 1.0, 1.0, 1.0, self.opacity)
+
+    Scale(self.layer4, width / 2.0, height / 2.0, 0)
+    Translate(self.layer4, width / 2.0, height / 2.0, 40.0)
+    Rotate(self.layer4, 0.0, 1.0, 0.0, 180)
+    SetColor(self.layer4, 1.0, 1.0, 1.0, self.opacity)
+
+    Scale(self.layer5, width / 2.0, height / 2.0, 0)
+    Translate(self.layer5, width / 2.0, height / 2.0, 50.0)
+    Rotate(self.layer5, 0.0, 1.0, 0.0, 180)
+    SetColor(self.layer5, 1.0, 1.0, 1.0, self.opacity)
+
+    Scale(self.flare, 400, 400, 0)
+    Translate(self.flare, width * (35 / 100), height * (85 / 100), 60.0)
+    Rotate(self.flare, 0.0, 1.0, 0.0, 180)
+    SetColor(self.flare, 1.0, 1.0, 1.0, self.opacity - self.flare_opacity)
+end
+
+function Level1:GetMushroomPositions()
+    return mushroom_positions
 end
 
 return Level1
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,264 @@
+
+#include <config.hpp>
+
+#ifdef BUILD_FOR_IOS
+#include <cstdlib>
+#define VK_USE_PLATFORM_IOS_MVK
+#include <vulkan/vulkan.hpp>
+
+#define NS_PRIVATE_IMPLEMENTATION
+#define UI_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#define MTK_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTLFX_PRIVATE_IMPLEMENTATION
+#include <Metal/Metal.hpp>
+#include <MetalFX/MetalFX.hpp>
+#include <MetalKit/MetalKit.hpp>
+#include <UIKit/UIKit.hpp>
+
 #include <application/application.hpp>
 #include <scripting/lua.hpp>
 #include <servicelocators/servicelocator.hpp>
 
-struct Entropia final : Entropy::EntryPoints::Application {
-    /**
-     * Initializes the application by retrieving the Lua scripting service from the service locator
-     * and executing the main Lua script.
-     * This method overrides the OnInit function in the base class.
-     */
-    void OnInit() override {
-        const ServiceLocator *sl = ServiceLocator::GetInstance();
-        const auto lua = sl->getService<ILua>();
-        lua->SetBasePath("../");
-        lua->Get()->script_file("../scripts/main.lua");
+extern "C" UI::ViewController *get_native_bounds(UI::View *view,
+                                                 UI::Screen *screen);
+extern "C" CGPoint touch();
+extern "C" float get_native_scale();
+extern "C" {
+void PlaySound(uint32_t index);
+void StopSound(uint32_t index);
+}
+
+class EntropyApplication final : public UI::ApplicationDelegate {
+public:
+  class MTKViewDelegate final : public MTK::ViewDelegate {
+  public:
+    MTKViewDelegate(
+        EntropyApplication *app,
+        const std::shared_ptr<
+            Entropy::Graphics::Vulkan::Renderers::VulkanRenderer> &renderer)
+        : ViewDelegate() {
+      const ServiceLocator *sl = ServiceLocator::GetInstance();
+      _app = app;
+      _physics2d = sl->getService<IPhysics2D>();
+      _lua = sl->getService<ILua>();
+      _timer = std::make_unique<Timer>(1.0f);
+      _renderer = renderer;
+      _lua->SetBasePath(GetProjectBasePath());
+      _lua->Get()->set_function("PlaySound",
+                                [](const uint32_t index) { PlaySound(index); });
+      _lua->Get()->set_function("StopSound",
+                                [](const uint32_t index) { StopSound(index); });
+      _lua->Get()->script_file(GetScriptsDir() + "main.lua");
     }
 
-    /**
-     * Handles the rendering of the application.
-     * This method overrides the OnRender function in the base class.
-     *
-     * @param screen_width
-     * @param screen_height
-     * @param deltaTime The time elapsed since the last frame, used for updating the rendering accordingly.
-     */
-    void OnRender(int screen_width, int screen_height, float deltaTime) override {
+    ~MTKViewDelegate() override = default;
+
+    void drawInMTKView(MTK::View *pView) override {
+
+      // Calculate frame delta time
+      const auto tick_time = _timer->GetTick();
+      deltaTime = tick_time - lastTick;
+      lastTick = tick_time;
+
+      _renderer->Render(_app->frame.size.width * get_native_scale(),
+                        _app->frame.size.height * get_native_scale(), 1.0, 1.0);
+
+      const auto on_render = _lua->Get()->get<sol::function>("OnRender");
+      const auto on_input = _lua->Get()->get<sol::function>("OnInput");
+
+      if (on_render.valid()) {
+        on_render(deltaTime, _app->frame.size.width * get_native_scale(),
+                  _app->frame.size.height * get_native_scale());
+      }
+
+      auto [x, y] = touch();
+      ServiceLocator::GetInstance()
+          ->getService<Entropy::Input::IMouse>()
+          ->position =
+          glm::vec2(x * get_native_scale(), y * get_native_scale());
+
+      //_physics2d->Get()->Step(timeStep, velocityIterations,
+      // positionIterations);
+      // physics2d->world->DebugDraw();
     }
+
+  private:
+    int velocityIterations = 16;
+    int positionIterations = 16;
+    float timeStep = 1.0f / 60.f;
+    float lastTick{};
+    float deltaTime{};
+    EntropyApplication *_app;
+    std::shared_ptr<Entropy::Graphics::Vulkan::Renderers::VulkanRenderer>
+        _renderer;
+    std::shared_ptr<IPhysics2D> _physics2d;
+    std::shared_ptr<ILua> _lua;
+    std::unique_ptr<Timer> _timer;
+  };
+
+  EntropyApplication() {
+    _autoreleasePool = NS::AutoreleasePool::alloc()->init();
+    _renderer = std::make_shared<
+        Entropy::Graphics::Vulkan::Renderers::VulkanRenderer>();
+  }
+
+  ~EntropyApplication() override {
+    _pMtkView->release();
+    _pWindow->release();
+    _pViewController->release();
+    _pDevice->release();
+    delete _pViewDelegate;
+    _autoreleasePool->release();
+  }
+
+  inline bool applicationDidFinishLaunching(UI::Application *pApp,
+                                            NS::Value *options) override {
+
+    frame = UI::Screen::mainScreen()->bounds();
+
+    const VkExtent2D extent = {static_cast<uint32_t>(frame.size.width),
+                               static_cast<uint32_t>(frame.size.height)};
+
+    _pWindow = UI::Window::alloc()->init(frame);
+    _pDevice = MTL::CreateSystemDefaultDevice();
+    _pMtkView = MTK::View::alloc()->init(frame, _pDevice);
+
+    _pViewController = get_native_bounds(
+        reinterpret_cast<UI::View *>(_pMtkView), UI::Screen::mainScreen());
+
+    _pMtkView->setColorPixelFormat(
+        MTL::PixelFormat::PixelFormatBGRA8Unorm_sRGB);
+    _pMtkView->setClearColor(MTL::ClearColor::Make(0.0, 1.0, 0.0, 0.0));
+
+    _pViewDelegate = new MTKViewDelegate(this, _renderer);
+    _pMtkView->setDelegate(_pViewDelegate);
+
+    auto *mtkView = reinterpret_cast<UI::View *>(_pMtkView);
+    mtkView->setAutoresizingMask(UI::ViewAutoresizingFlexibleWidth |
+                                 UI::ViewAutoresizingFlexibleHeight);
+
+    _pViewController->view()->addSubview(mtkView);
+    _pWindow->setRootViewController(_pViewController);
+
+    _pWindow->makeKeyAndVisible();
+
+    CA::MetalLayer *layer = _pMtkView->currentDrawable()->layer();
+
+    ServiceLocator *sl = ServiceLocator::GetInstance();
+
+    VkIOSSurfaceCreateInfoMVK createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_IOS_SURFACE_CREATE_INFO_MVK;
+    createInfo.pNext = nullptr;
+    createInfo.flags = 0;
+    createInfo.pView = layer;
+
+    if (vkCreateIOSSurfaceMVK(sl->getService<IVulkanInstance>()->Get(),
+                              &createInfo, nullptr, &_surface) != VK_SUCCESS) {
+      spdlog::error("Failed to create a window surface for platform 'iOS'");
+      return false;
+    }
+
+    _renderer->_swapchain->Build(_surface, extent, nullptr);
+
+    _renderer->_renderPass->RecreateDepthBuffer(
+        extent.width * get_native_scale(), extent.height * get_native_scale());
+    _renderer->_renderPass->CreateFrameBuffers(
+        sl->getService<ISwapchain>(), extent.width * get_native_scale(),
+        extent.height * get_native_scale());
+
+    return true;
+  }
+
+public:
+  inline void Run() { UI::ApplicationMain(0, 0, this); }
+  CGRect frame{};
+
+private:
+  UI::Window *_pWindow = nullptr;
+  MTK::View *_pMtkView = nullptr;
+  MTL::Device *_pDevice = nullptr;
+  MTKViewDelegate *_pViewDelegate = nullptr;
+  NS::AutoreleasePool *_autoreleasePool = nullptr;
+  UI::ViewController *_pViewController = nullptr;
+  std::shared_ptr<Entropy::Graphics::Vulkan::Renderers::VulkanRenderer>
+      _renderer;
+  VkSurfaceKHR _surface = VK_NULL_HANDLE;
 };
 
 int main() {
-    auto game = Entropia();
-    game.Run();
-    return EXIT_SUCCESS;
+
+  ServiceLocator *sl = ServiceLocator::GetInstance();
+
+  // Vulkan services
+  sl->registerService(std::make_shared<VulkanInstance>());
+  sl->registerService(std::make_shared<PhysicalDevice>());
+  sl->registerService(std::make_shared<LogicalDevice>());
+  sl->registerService(std::make_shared<Allocator>());
+  sl->registerService(std::make_shared<SwapChain>());
+  sl->registerService(
+      std::make_shared<Entropy::Graphics::Vulkan::CommandPools::CommandPool>());
+  sl->registerService(std::make_shared<DescriptorPool>());
+  sl->registerService(
+      std::make_shared<Entropy::Graphics::Vulkan::Caches::PipelineCache>());
+
+  // Cameras
+  sl->registerService(std::make_shared<CameraManager>());
+  const auto cameraManager = sl->getService<ICameraManager>();
+  const auto camera = new OrthographicCamera();
+  cameraManager->currentCamera = camera;
+
+  // 2D Physics
+  sl->registerService(std::make_shared<Entropy::Physics::Physics2D>());
+
+  // ECS
+  sl->registerService(std::make_shared<World>());
+
+  // Scripting
+  sl->registerService(std::make_shared<Lua>());
+
+  // Input
+  sl->registerService(std::make_shared<Entropy::Input::Keyboard>());
+  sl->registerService(std::make_shared<Entropy::Input::Mouse>());
+
+  auto app = EntropyApplication();
+  app.Run();
+
+  return EXIT_SUCCESS;
 }
+
+#else
+struct Entropia final : Entropy::Entrypoints::Application {
+  /**
+   * Initializes the application by retrieving the Lua scripting service from
+   * the service locator and executing the main Lua script. This method
+   * overrides the OnInit function in the base class.
+   */
+  void OnInit() override {
+    const ServiceLocator *sl = ServiceLocator::GetInstance();
+    const auto lua = sl->getService<ILua>();
+    lua->SetBasePath("../");
+    lua->Get()->script_file("../scripts/main.lua");
+  }
+
+  /**
+   * Handles the rendering of the application.
+   * This method overrides the OnRender function in the base class.
+   *
+   * @param screen_width
+   * @param screen_height
+   * @param deltaTime The time elapsed since the last frame, used for updating
+   * the rendering accordingly.
+   */
+  void OnRender(int screen_width, int screen_height, float deltaTime) override {
+  }
+};
+
+int main() {
+  auto game = Entropia();
+  game.Run();
+  return EXIT_SUCCESS;
+}
+
+#endif
